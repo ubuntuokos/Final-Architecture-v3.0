193bc1698fb4ea4cc6513f75f552e9a9e3210a5b
István Horváth <ubuntuokos@gmail.com>
2026-09-13 10:20:40 +0200

    FA3 GUI: launch responsive operations shell

--- apps/fa3-control-center/src/main.cpp
+++ apps/fa3-control-center/src/main.cpp
@@ -1,5 +1,8 @@
 #include "Fa3RepositoryModel.h"
 #include "SettingsStore.h"
+#include "ResourceTelemetry.h"
+#include "LlmfitClient.h"
+#include "JournalReader.h"
 
 #include <QApplication>
 #include <QCoreApplication>
@@ -15,21 +18,25 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName("FA3ControlCenter");
-    QCoreApplication::setApplicationVersion("0.4.0");
+    QCoreApplication::setApplicationVersion("0.5.0");
     QCoreApplication::setOrganizationName("FinalArchitecture");
     QCoreApplication::setOrganizationDomain("fa3.local");
     QApplication::setApplicationDisplayName("FA3 Control Center");
 
     Fa3RepositoryModel repository;
     SettingsStore settings;
+    ResourceTelemetry telemetry;
+    LlmfitClient llmfit;
+    JournalReader journal;
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("fa3Repository", &repository);
     engine.rootContext()->setContextProperty("fa3Settings", &settings);
-    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/FA3/ControlCenter/AnimationAwareAppShell.qml")));
+    engine.rootContext()->setContextProperty("fa3ResourceTelemetry", &telemetry);
+    engine.rootContext()->setContextProperty("llmfitClient", &llmfit);
+    engine.rootContext()->setContextProperty("fa3Journal", &journal);
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/FA3/ControlCenter/OperationsAwareAppShell.qml")));
 
-    if (engine.rootObjects().isEmpty()) {
-        return 2;
-    }
+    if (engine.rootObjects().isEmpty()) return 2;
     return app.exec();
 }
