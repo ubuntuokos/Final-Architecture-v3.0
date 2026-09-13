729491705cbb3bb8f32d4735bc7da35bc6bd1325
István Horváth <ubuntuokos@gmail.com>
2026-09-13 10:23:02 +0200

    FA3 GUI: preserve application version contract

--- apps/fa3-control-center/src/main.cpp
+++ apps/fa3-control-center/src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName("FA3ControlCenter");
-    QCoreApplication::setApplicationVersion("0.5.0");
+    QCoreApplication::setApplicationVersion("0.4.0");
     QCoreApplication::setOrganizationName("FinalArchitecture");
     QCoreApplication::setOrganizationDomain("fa3.local");
     QApplication::setApplicationDisplayName("FA3 Control Center");
