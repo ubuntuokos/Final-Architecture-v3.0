0b8bcef33b2bf060ea2c387b8321f49cebfcadba
István Horváth <ubuntuokos@gmail.com>
2026-09-14 18:43:54 +0200

    FA3 GUI: expose live interpreter runtime to QML

--- apps/fa3-control-center/src/main.cpp
+++ apps/fa3-control-center/src/main.cpp
@@ -1,6 +1,7 @@
 #include "ChatFileService.h"
 #include "Fa3RepositoryModel.h"
 #include "JournalService.h"
+#include "LanguageInterpreterService.h"
 #include "ModelLibraryService.h"
 #include "McpControlService.h"
 #include "PreferenceStore.h"
@@ -47,6 +48,7 @@ int main(int argc, char *argv[])
     ChatFileService chatFiles;
     ModelLibraryService modelLibrary;
     McpControlService mcpControl;
+    LanguageInterpreterService interpreter;
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("fa3Repository", &repository);
@@ -56,6 +58,7 @@ int main(int argc, char *argv[])
     engine.rootContext()->setContextProperty("fa3ChatFiles", &chatFiles);
     engine.rootContext()->setContextProperty("fa3ModelLibrary", &modelLibrary);
     engine.rootContext()->setContextProperty("fa3McpControl", &mcpControl);
+    engine.rootContext()->setContextProperty("fa3Interpreter", &interpreter);
     engine.load(QUrl(QStringLiteral("qrc:/qt/qml/FA3/ControlCenter/Main.qml")));
 
     if (engine.rootObjects().isEmpty()) {
